88e5959eee70f21a0789d3d2a5861b6032c99f13
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-12-12 12:15:38 +0900

    Add code to move document vectors to Wikipedia folder

---
+++ Util/move_document_vectors.py
@@ -0,0 +1,29 @@
+import argparse
+from pathlib import Path
+from tqdm import tqdm
+
+def main(args):
+    vectors_dirname:str=args.vectors_dirname
+    wikipedia_data_root_dirname:str=args.wikipedia_data_root_dirname
+
+    vectors_dir=Path(vectors_dirname)
+    wikipedia_data_root_dir=Path(wikipedia_data_root_dirname)
+
+    vector_files=vectors_dir.glob("*.pt")
+    vector_files=len(vector_files)
+
+    for vector_file in tqdm(vector_files):
+        title_hash=vector_file.stem
+        wikipedia_data_dir=wikipedia_data_root_dir.joinpath(title_hash)
+
+        dst_file=wikipedia_data_dir.joinpath("vector.pt")
+
+        vector_file.rename(dst_file)
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--vectors_dirname",type=str,default="../Data/WikipediaVector")
+    parser.add_argument("--wikipedia_data_root_dirname",type=str,default="../Data/Wikipedia")
+    args=parser.parse_args()
+
+    main(args)

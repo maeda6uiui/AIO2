e7231f20cc9820db223753fe0f536eb9e2c6f033
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-12-12 16:32:31 +0900

    Update

--- Retriever/modeling.py
+++ Retriever/modeling.py
@@ -232,6 +232,7 @@ def eval(
         #QuestionとDocumentの関連度を取得する
         with torch.no_grad():
             logits=score_calculator(question_vectors,document_vectors)
+            logits=torch.squeeze(logits)
             
         t_corresponding_flags=torch.as_tensor(corresponding_flags,dtype=torch.float,device=device)
         loss=criterion(logits,t_corresponding_flags)

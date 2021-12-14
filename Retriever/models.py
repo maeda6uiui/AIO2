96b5f604fc0c58d5e28e6d0813966e748b8183ec
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-12-14 14:43:48 +0900

    Update model

--- Retriever/models.py
+++ Retriever/models.py
@@ -7,9 +7,11 @@ class RelevanceScoreCalculator(nn.Module):
 
         self.main=nn.Sequential(
             nn.Linear(dim_feature_vector*2,dim_feature_vector),
-            nn.PReLU(),
-            nn.Linear(dim_feature_vector,100),
-            nn.PReLU(),
+            nn.Mish(),
+            nn.Linear(dim_feature_vector,int(dim_feature_vector/2)),
+            nn.Mish(),
+            nn.Linear(int(dim_feature_vector/2),100),
+            nn.Mish(),
             nn.Linear(100,1),
             nn.Sigmoid()
         )

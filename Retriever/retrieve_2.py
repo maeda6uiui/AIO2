e9b0d7cad948f749c12df0a432df57b1d7eee87e
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2021-12-19 16:49:56 +0900

    Update

--- Retriever/retrieve_2.py
+++ Retriever/retrieve_2.py
@@ -1,12 +1,10 @@
 import argparse
 import json
 import logging
-import cupy as cp
 import numpy as np
 import torch
 from pathlib import Path
 from transformers import BertConfig
-from cupyx.scipy.sparse import csr_matrix as csr_gpu
 from scipy import sparse
 from tqdm import tqdm
 from typing import Dict,List,Tuple
@@ -23,7 +21,46 @@ logging.basicConfig(format=logging_fmt)
 logger=logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
 
-device=torch.device("cuda")
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def convert_csr_to_tensor(csr:sparse.csr_matrix)->torch.FloatTensor:
+    coo=csr.tocoo()
+
+    values=coo.data
+    indices=np.vstack((coo.row,coo.col))
+
+    i=torch.LongTensor(indices)
+    v=torch.FloatTensor(values)
+    shape=coo.shape
+
+    tensor=torch.sparse_coo_tensor(i,v,torch.Size(shape))
+
+    return tensor
+
+def load_corpus_tensors(corpus_tensors_dir:Path)->List[torch.FloatTensor]:
+    info={}
+    info_file=corpus_tensors_dir.joinpath("info.txt")
+    with info_file.open("r") as r:
+        for line in r:
+            key,value=line.split("=")
+            info[key]=value
+
+    num_divisions=int(info["num_divisions"])
+
+    corpus_tensors=[]
+    for i in range(num_divisions):
+        indices_file=corpus_tensors_dir.joinpath("indices_{}.pt".format(i))
+        values_file=corpus_tensors_dir.joinpath("values_{}.pt".format(i))
+        size_file=corpus_tensors_dir.joinpath("size_{}.pt".format(i))
+
+        indices=torch.load(indices_file,map_location=torch.device("cpu"))
+        values=torch.load(values_file,map_location=torch.device("cpu"))
+        size=torch.load(size_file,map_location=torch.device("cpu"))
+
+        corpus_tensor=torch.sparse_coo_tensor(indices,values,size,device=device)
+        corpus_tensors.append(corpus_tensor)
+
+    return corpus_tensors
 
 def load_questions(
     samples_filepath:str,
@@ -99,21 +136,26 @@ def retrieve_by_score_calculator(
 
     all_scores=torch.squeeze(all_scores)
     
-    return all_scores.cpu().detach().numpy()
+    return all_scores
 
 def retrieve_by_tf_idf(
     question:str,
     genkei_extractor:GenkeiExtractor,
     tf_idf_calculator:TFIDFCalculator,
-    corpus_matrix)->np.ndarray:
+    corpus_tensors:List[torch.FloatTensor])->np.ndarray:
     q_genkeis=genkei_extractor.extract_genkeis_from_text(question)
     q_genkeis=" ".join(q_genkeis)
 
     q_vector=tf_idf_calculator.transform_query(q_genkeis)
-    q_vector=csr_gpu(q_vector,dtype=np.float32)
+    q_vector=convert_csr_to_tensor(q_vector).to_dense()
+    q_vector=torch.squeeze(q_vector)
+    q_vector=q_vector.to(device)
 
-    cosine_similarities=corpus_matrix.dot(q_vector.T).flatten()
-    cosine_similarities=cp.asnumpy(cosine_similarities)
+    cosine_similarities=torch.empty(0,dtype=torch.float)
+
+    for corpus_tensor in corpus_tensors:
+        cs_temp=torch.mv(corpus_tensor,q_vector)
+        cosine_similarities=torch.cat([cosine_similarities,cs_temp.cpu()],dim=0)
 
     return cosine_similarities
 
@@ -134,7 +176,7 @@ def main(args):
     bert_model_name:str=args.bert_model_name
     score_calculator_filepath:str=args.score_calculator_filepath
     mecab_dictionary_dirname:str=args.mecab_dictionary_dirname
-    corpus_matrix_filepath:str=args.corpus_matrix_filepath
+    corpus_tensors_dirname:str=args.corpus_tensors_dirname
     vectorizer_filepath:str=args.vectorizer_filepath
     batch_size:int=args.batch_size
     k:int=args.k
@@ -161,8 +203,8 @@ def main(args):
 
     genkei_extractor=GenkeiExtractor(mecab_dictionary_dirname,1024*1024)
 
-    corpus_matrix=sparse.load_npz(corpus_matrix_filepath)
-    corpus_matrix=csr_gpu(corpus_matrix,dtype=np.float32)
+    corpus_tensors_dir=Path(corpus_tensors_dirname)
+    corpus_tensors=load_corpus_tensors(corpus_tensors_dir)
 
     tf_idf_calculator=TFIDFCalculator()
     tf_idf_calculator.load_vectorizer(vectorizer_filepath)
@@ -197,15 +239,14 @@ def main(args):
                 question,
                 genkei_extractor,
                 tf_idf_calculator,
-                corpus_matrix)
+                corpus_tensors
+            )
 
-            scores=calculator_scores*tf_idf_scores
+            scores=torch.mul(calculator_scores,tf_idf_scores)
 
-            top_k_indices=np.argpartition(scores,-k)[-k:]
-            top_k_indices=top_k_indices[np.argsort(-scores[top_k_indices])]
-
-            top_k_scores=scores[top_k_indices]
-            top_k_scores=top_k_scores.tolist()
+            top_k_indices,top_k_scores=torch.topk(scores,k=k)
+            top_k_indices=top_k_indices.cpu().detach().tolist()
+            top_k_scores=top_k_scores.cpu().detach().tolist()
 
             top_k_titles=[]
             for i in range(k):
@@ -241,7 +282,7 @@ if __name__=="__main__":
     parser.add_argument("--bert_model_name",type=str,default="cl-tohoku/bert-base-japanese-whole-word-masking")
     parser.add_argument("--score_calculator_filepath",type=str,default="../Data/Retriever/BERT/score_calculator.pt")
     parser.add_argument("--mecab_dictionary_dirname",type=str,default="/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
-    parser.add_argument("--corpus_matrix_filepath",type=str,default="../Data/Retriever/TF-IDF/corpus_matrix.npz")
+    parser.add_argument("--corpus_tensors_dirname",type=str,default="../Data/Retriever/TF-IDF/CorpusTensor")
     parser.add_argument("--vectorizer_filepath",type=str,default="../Data/Retriever/TF-IDF/vectorizer.pkl")
     parser.add_argument("--batch_size",type=int,default=512)
     parser.add_argument("--k",type=int,default=100)

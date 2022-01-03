1f87e8c621a54f03d7196e396d26802911222acf
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2022-01-03 20:56:17 +0900

    Update model

--- Reader/RoBERTa/models.py
+++ Reader/RoBERTa/models.py
@@ -11,24 +11,12 @@ class Reader(nn.Module):
         config=AutoConfig.from_pretrained(model_name)
 
         self.seq_span=nn.Sequential(
-            nn.Linear(config.hidden_size*4,config.hidden_size*2),
-            nn.Mish(),
-            nn.Linear(config.hidden_size*2,config.hidden_size),
-            nn.Mish(),
-            nn.Linear(config.hidden_size,256),
-            nn.Mish(),
             nn.Dropout(p=0.1),
-            nn.Linear(256,2)
+            nn.Linear(config.hidden_size,2)
         )
         self.seq_plausibility=nn.Sequential(
-            nn.Linear(config.hidden_size*4,config.hidden_size*2),
-            nn.Mish(),
-            nn.Linear(config.hidden_size*2,config.hidden_size),
-            nn.Mish(),
-            nn.Linear(config.hidden_size,256),
-            nn.Mish(),
             nn.Dropout(p=0.1),
-            nn.Linear(256,1),
+            nn.Linear(config.hidden_size,1),
             nn.Sigmoid()
         )
 
@@ -48,20 +36,16 @@ class Reader(nn.Module):
         }
         outputs=self.model(**inputs)
 
-        hidden_states=outputs["hidden_states"][-4:]
+        hidden_states=outputs["hidden_states"][-1]  #(N, sequence_length, hidden_size)
 
-        concat_hidden_states=hidden_states[0]   #(N, sequence_length, hidden_size) → (N, sequence_length, hidden_size*4)
-        for i in range(1,4):
-            concat_hidden_states=torch.cat([concat_hidden_states,hidden_states[i]],dim=2)
-
-        span_logits=self.seq_span(concat_hidden_states) #(N, sequence_length, 2)
+        span_logits=self.seq_span(hidden_states) #(N, sequence_length, 2)
         start_logits,end_logits=torch.split(span_logits,1,dim=2)    #(N, sequence_length, 1)
 
         start_logits=torch.squeeze(start_logits)    #(N, sequence_length)
         end_logits=torch.squeeze(end_logits)    #(N, sequence_length)
 
-        cls_vectors=concat_hidden_states[:,0,:] #(N, hidden_size)
-        plausibility_scores=self.seq_plausibility(cls_vectors)  #(M, 1)
+        cls_vectors=hidden_states[:,0,:] #(N, hidden_size)
+        plausibility_scores=self.seq_plausibility(cls_vectors)  #(N, 1)
         plausibility_scores=torch.squeeze(plausibility_scores)  #(N)
 
         loss=None

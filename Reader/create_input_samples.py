c1e84b82f45244338659f87c486d8c908ee9f773
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2022-01-16 16:42:32 +0900

    Update

--- Reader/create_input_samples.py
+++ Reader/create_input_samples.py
@@ -85,7 +85,8 @@ def main(args):
     retrieval_results_filepath:str=args.retrieval_results_filepath
     wikipedia_data_root_dirname:str=args.wikipedia_data_root_dirname
     bert_model_name:str=args.bert_model_name
-    output_filepath:str=args.output_filepath
+    positive_output_filepath:str=args.positive_output_filepath
+    negative_output_filepath:str=args.negative_output_filepath
     context_max_length:int=args.context_max_length
     limit_num_titles:int=args.limit_num_titles
 
@@ -100,42 +101,67 @@ def main(args):
 
     logger.info("データを作成しています...")
 
-    with open(output_filepath,"w") as w:
-        for qid,question,this_answers,this_top_k_titles in tqdm(zip(qids,questions,answers,top_k_titles),total=len(qids)):
-            for title in this_top_k_titles:
-                title_hash=get_md5_hash(title)
-                text_file=wikipedia_data_root_dir.joinpath(title_hash,"text.txt")
-                with text_file.open("r") as r:
-                    context=r.read()
+    positives=[]
+    negatives=[]
 
-                for answer in this_answers:
-                    start_indices,end_indices=get_answer_indices_from_context(
-                        question,
-                        answer,
-                        context,
-                        tokenizer,
-                        config.max_position_embeddings,
-                        context_max_length
-                    )
-                    if len(start_indices)==0:
-                        continue
+    for qid,question,this_answers,this_top_k_titles in tqdm(zip(qids,questions,answers,top_k_titles),total=len(qids)):
+        for title in this_top_k_titles:
+            title_hash=get_md5_hash(title)
+            text_file=wikipedia_data_root_dir.joinpath(title_hash,"text.txt")
+            with text_file.open("r") as r:
+                context=r.read()
 
-                    ranges:List[str]=[]
-                    for start_index,end_index in zip(start_indices,end_indices):
-                        range="{}-{}".format(start_index,end_index)
-                        ranges.append(range)
+            answer_contained=False
+            for answer in this_answers:
+                start_indices,end_indices=get_answer_indices_from_context(
+                    question,
+                    answer,
+                    context,
+                    tokenizer,
+                    config.max_position_embeddings,
+                    context_max_length
+                )
+                if len(start_indices)==0:
+                    continue
+                
+                ranges:List[str]=[]
+                for start_index,end_index in zip(start_indices,end_indices):
+                    range="{}-{}".format(start_index,end_index)
+                    ranges.append(range)
 
-                    data={
-                        "qid":qid,
-                        "question":question,
-                        "answer":answer,
-                        "article_title":title,
-                        "answer_ranges":ranges
-                    }
-                    line=json.dumps(data,ensure_ascii=False)
+                data={
+                    "qid":qid,
+                    "question":question,
+                    "answer":answer,
+                    "article_title":title,
+                    "answer_ranges":ranges
+                }
+                positives.append(data)
+                answer_contained=True
 
-                    w.write(line)
-                    w.write("\n")
+            if answer_contained==False:
+                data={
+                    "qid":qid,
+                    "question":question,
+                    "answer":this_answers[0],
+                    "article_title":title,
+                    "answer_ranges":["0-0"]
+                }
+                negatives.append(data)
+
+    logger.info("作成したデータをファイルに書き込んでいます...")
+
+    with open(positive_output_filepath,"w") as w:
+        for positive in positives:
+            line=json.dumps(positive,ensure_ascii=False)
+            w.write(line)
+            w.write("\n")
+
+    with open(negative_output_filepath,"w") as w:
+        for negative in negatives:
+            line=json.dumps(negative,ensure_ascii=False)
+            w.write(line)
+            w.write("\n")
 
     logger.info("処理が完了しました")
 
@@ -144,7 +170,8 @@ if __name__=="__main__":
     parser.add_argument("--retrieval_results_filepath",type=str,default="../Data/Retriever/train_top_ks.jsonl")
     parser.add_argument("--wikipedia_data_root_dirname",type=str,default="../Data/Wikipedia")
     parser.add_argument("--bert_model_name",type=str,default="cl-tohoku/bert-base-japanese-whole-word-masking")
-    parser.add_argument("--output_filepath",type=str,default="../Data/Reader/train_samples.jsonl")
+    parser.add_argument("--positive_output_filepath",type=str,default="../Data/Reader/train_positive_samples.jsonl")
+    parser.add_argument("--negative_output_filepath",type=str,default="../Data/Reader/train_negative_samples.jsonl")
     parser.add_argument("--context_max_length",type=int,default=3000)
     parser.add_argument("--limit_num_titles",type=int,default=100)
     args=parser.parse_args()
